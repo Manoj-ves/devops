3f426970fe0fa5715bb9303a196fef11cf3dfaf0
Manoj-ves <2023.manoj.sudalai@ves.ac.in>
2024-04-08 12:53:43 +0530

    app

---
+++ src/devops_practical_exam/App.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package devops_practical_exam;
+
+/**
+ *
+ * @author user
+ */
+public class App {
+    public static void main(String[] args){
+        System.out.println("Result : " + 5* 5);
+    }
+    
+}
